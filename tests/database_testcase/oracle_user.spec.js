fc97faf717a4e618e9d95ca28b6764da20a7c91a
thanasornsawan <thanasornsawan.v@doppio-tech.com>
2024-12-30 17:45:02 +0200

    update

--- tests/database_testcase/oracle_user.spec.js
+++ tests/database_testcase/oracle_user.spec.js
@@ -2,22 +2,29 @@ const { test, expect } = require('@playwright/test');
 const { DatabaseConnection } = require('../../databases/oracle/dbConnection');
 const { UserQueries } = require('../../databases/oracle/userQueries');
 const testData = require('../../data/testData.json');
+const allure = require('allure-playwright');
 
 test.beforeAll(async () => {
-    await DatabaseConnection.initialize();
-  });
+  await DatabaseConnection.initialize();
+});
+
+test.afterAll(async () => {
+  await DatabaseConnection.close();
+});
+
+test('should retrieve user credentials by site @dbtest', async ({ page }) => {
+  const testUser = testData.testUsers[0];
   
-  test.afterAll(async () => {
-    await DatabaseConnection.close();
-  });
-  
-  test('should retrieve user credentials by site', async () => {
-    const testUser = testData.testUsers[0];
+  await allure.step('Query user account', async () => {
     const userAccount = await UserQueries.getUserAccountBySite(testUser.site);
+    
+    await allure.attachment('User Account Details', JSON.stringify(userAccount, null, 2), 'application/json');
     
     expect(userAccount).toMatchObject({
       USERNAME: testUser.username,
       PASSWORD: testUser.password,
       SITE: testUser.site
     });
-  });
+
+    console.log('Retrieved username:', userAccount.USERNAME);
+    console.log('Retrieved password:', userAccount.PASSW
